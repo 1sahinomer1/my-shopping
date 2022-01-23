b22bae83365f458f84d205f0cd0716b2941fdb01
Ömer Şahin <63051255+1sahinomer1@users.noreply.github.com>
2022-01-23 18:23:52 +0300

    add pagination

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,50 @@
+import { Products } from 'components';
+import { useEffect, useState } from 'react';
+import ReactPaginate from 'react-paginate';
+import { useSelector } from 'react-redux';
+import { AppState } from 'store';
+import { Product } from 'types/product';
+
+const Pagination = () => {
+  const [currentItems, setCurrentItems] = useState<Product[]>();
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+  const data = useSelector((state: AppState) => state.products.data);
+
+  const handlePageClick = (event: any) => {
+    const newOffset = (event.selected * 16) % data.length;
+    setItemOffset(newOffset);
+  };
+
+  useEffect(() => {
+    const endOffset = itemOffset + 16;
+    setCurrentItems(data.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(data.length / 16));
+  }, [data, itemOffset]);
+  return (
+    <div>
+      <Products data={currentItems} />
+      <ReactPaginate
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        marginPagesDisplayed={2}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakLabel="..."
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+      />
+    </div>
+  );
+};
+
+export default Pagination;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
 export { default as Header } from './Header/Header';
+export { default as ProductCard } from './ProductCard/ProductCard';
+export { default as Products } from './Products/Products';
+export { default as Pagination } from './Pagination/Pagination';
