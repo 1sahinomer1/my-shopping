e438e4be6156e19b381d5e208a45d675984bbea9
Ömer Şahin <63051255+1sahinomer1@users.noreply.github.com>
2022-01-23 18:25:26 +0300

    update home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,27 @@
+import { Pagination } from 'components';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getProductsAll } from 'store/actions/productActions';
+import styled from 'styled-components';
+
 const Home = () => {
-  return <div></div>;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProductsAll());
+  }, []);
+
+  return (
+    <Wrapper>
+      <p>filter</p>
+      <Pagination />
+    </Wrapper>
+  );
 };
 
 export default Home;
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 20% 80%;
+`;
